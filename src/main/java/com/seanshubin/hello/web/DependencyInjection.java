fa894ea86ad8b7a0f4cf7987ac0bc5abae44f021
Sean Shubin <sshubin@cj.com>
2016-11-16 19:48:04 -0800

    Comments

--- src/main/java/com/seanshubin/hello/web/DependencyInjection.java
+++ src/main/java/com/seanshubin/hello/web/DependencyInjection.java
@@ -4,11 +4,11 @@ import javax.servlet.ServletConfig;
 
 class DependencyInjection {
     DependencyInjection(ServletConfig servletConfig) {
-        // Servlet config not needed right now,
+        // ServletConfig is not needed right now,
         // I only left this in to demonstrate how this information
-        // would be passed along from the EntryPointServlet if it was needed.
+        // would be passed along from EntryPointServlet if it was needed.
     }
 
-    final Handler dispatcher = new DispatchHandler();
+    private final Handler dispatcher = new DispatchHandler();
     final HttpServletRequestHandler httpServletRequestHandler = new TopLevelHttpServletRequestHandler(dispatcher);
 }
