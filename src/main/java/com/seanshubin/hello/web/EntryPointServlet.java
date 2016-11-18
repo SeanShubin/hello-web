5953e08daa2fcf17215a4f5ac54cfffc13351a7c
Sean Shubin <sshubin@cj.com>
2016-11-17 17:34:13 -0800

    Made EntryPointServlet public

--- src/main/java/com/seanshubin/hello/web/EntryPointServlet.java
+++ src/main/java/com/seanshubin/hello/web/EntryPointServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-class EntryPointServlet extends HttpServlet {
+public class EntryPointServlet extends HttpServlet {
     private static DependencyInjection dependencyInjection;
 
     @Override
