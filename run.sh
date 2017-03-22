75cc6e257dd2b274e2e714775e328516b8d67940
Sean Shubin <sshubin@cj.com>
2017-03-22 15:46:17 -0700

    Download jetty runner instead of version controlling it

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+wget -nc -O jetty-runner.jar http://central.maven.org/maven2/org/eclipse/jetty/jetty-runner/9.4.3.v20170317/jetty-runner-9.4.3.v20170317.jar
+chmod +x ./jetty-runner.jar
+
 echo http://localhost:8080/hello?target=world
 echo http://localhost:8080/length?target=world
 java -jar jetty-runner.jar target/hello-web-1.0.war
